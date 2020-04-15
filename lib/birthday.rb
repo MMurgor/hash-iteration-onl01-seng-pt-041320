393243d835f7e80c6194d4a0c9a2256284b09014
Romena Murgor <romenamurgor@gmail.com>
2020-04-15 01:15:44 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,8 +4,29 @@
 #   "Amanda" => 27
 # }
 
-def happy_birthday(birthday_kids)
-  # add your code snippet here!
+def "happy_birthday(birthday_kids)
+  birthday_kids.each
+do  |Timmy, 9|
+  puts "Happy Birthday #
+  {Timmy}! You are now #{9} years old"
 end
+end
+
+ def "happy_birthday(birthday_kids)
+  birthday_kids.each
+do  |Sarah, 6|
+  puts "Happy Birthday #
+  {Sarah}! You are now #{6} years old"
+ end
+ end
+
+def"happy_birthday(birthday_kids)
+  birthday_kids.each
+do  |Amanda, 9|
+  puts "Happy Birthday #
+  {Amanda}! You are now #{27} years old"
+end 
+end 
+
 
 
--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,5 +7,39 @@
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
+  passengers.each do |suite_a, |"Amanda Presley"|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
 end
+end
+
+def select_winner(passengers)
+  passengers.each do |suite_b, |"Seymour Hoffman"|
+  if suite == :suite_b && name.start_with?("B")
+    winner = name
+  end
+end
+
+def select_winner(passengers)
+  passengers.each do |suite_c, |"Alfred Tennyson"|
+  if suite == :suite_c && name.start_with?("C")
+    winner = name
+  end
+end
+
+winner
+def select_winner(passengers)
+  passengers.each do |suite_d, |"Charlie Chaplin"|
+  if suite == :suite_d && name.start_with?("D")
+    winner = name
+  end
+end
+ winner 
+
+def select_winner(passengers)
+  passengers.each do |suite_e, |"Crumpet the Elf"|
+  if suite == :suite_e && name.start_with?("E")
+    winner = name
+  end
+end
+winner
