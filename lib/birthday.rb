0cbe4442703b52c906a7204745b2e60ad9535575
Romena Murgor <romenamurgor@gmail.com>
2020-04-16 23:55:37 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,32 +1,9 @@
-# birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
-#   "Amanda" => 27
-# }
-
-def "happy_birthday(birthday_kids)
-  birthday_kids.each
-do  |Timmy, 9|
-  puts "Happy Birthday #
-  {Timmy}! You are now #{9} years old"
-end
+require "pry"
+def happy_birthday(birthday_kids)
+   birthday_kids.each do  |name,age|
+    puts "Happy Birthday #{name}! You are now #{age} years old!"
+    #binding.pry
+  end
 end
 
- def "happy_birthday(birthday_kids)
-  birthday_kids.each
-do  |Sarah, 6|
-  puts "Happy Birthday #
-  {Sarah}! You are now #{6} years old"
- end
- end
-
-def"happy_birthday(birthday_kids)
-  birthday_kids.each
-do  |Amanda, 9|
-  puts "Happy Birthday #
-  {Amanda}! You are now #{27} years old"
-end 
-end 
-
-
-
+ 
--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,44 +1,10 @@
-# passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
-# suite_e: "Crumpet the Elf"
-# }
-
+require "pry"
 def select_winner(passengers)
-  passengers.each do |suite_a, |"Amanda Presley"|
-  if suite == :suite_a && name.start_with?("A")
-    winner = name
-end
-end
-
-def select_winner(passengers)
-  passengers.each do |suite_b, |"Seymour Hoffman"|
-  if suite == :suite_b && name.start_with?("B")
-    winner = name
+  passengers.each do |suite,name|
+    #binding.pry
+    if suite == :suite_a && name.start_with?("A")
+      return name
+    end
   end
 end
 
-def select_winner(passengers)
-  passengers.each do |suite_c, |"Alfred Tennyson"|
-  if suite == :suite_c && name.start_with?("C")
-    winner = name
-  end
-end
-
-
-def select_winner(passengers)
-  passengers.each do |suite_d, |"Charlie Chaplin"|
-  if suite == :suite_d && name.start_with?("D")
-    winner = name
-  end
-end
-  
-
-def select_winner(passengers)
-  passengers.each do |suite_e, |"Crumpet the Elf"|
-  if suite == :suite_e && name.start_with?("E")
-    winner = name
-  end
-end 
