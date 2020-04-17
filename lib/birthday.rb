c277bd1b5cc976118974772494eee5606a43335c
Romena Murgor <romenamurgor@gmail.com>
2020-04-17 00:52:26 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,8 +1,6 @@
-require "pry"
 def happy_birthday(birthday_kids)
    birthday_kids.each do  |name,age|
     puts "Happy Birthday #{name}! You are now #{age} years old!"
-    #binding.pry
   end
 end
 
--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,7 +1,5 @@
-require "pry"
 def select_winner(passengers)
   passengers.each do |suite,name|
-    #binding.pry
     if suite == :suite_a && name.start_with?("A")
       return name
     end
